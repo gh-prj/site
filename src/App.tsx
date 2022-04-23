723d140216b779c8f16cfabbd37818e1472e567f
gh-prj <ghprj@yandex.ru>
2022-04-23 03:53:41 +0300

    Add Test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
-      Hello World!
+      Hello World!<hr />
+      <Test />
     </div>
   );
 }
