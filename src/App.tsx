361862647a2fbf8eb68e8c655f62c831881e286d
gh-prj <ghprj@yandex.ru>
2022-04-29 14:41:27 +0300

    Revert "Add Test component"
    This reverts commit 723d140216b779c8f16cfabbd37818e1472e567f.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import './App.css';
-import Test from './components/Test';
 
 function App() {
   return (
     <div className="App">
-      Hello World!<hr />
-      <Test />
+      Hello World!
     </div>
   );
 }
