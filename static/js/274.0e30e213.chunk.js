8bd48855215e90a9d475e65ca8df2e36b1400ac9
gh-prj <ghprj@yandex.ru>
2022-05-10 15:11:46 +0300

    Updates

---
+++ static/js/274.0e30e213.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgh_react_site=self.webpackChunkgh_react_site||[]).push([[274],{274:function(e,t,s){s.r(t);s(791);var c=s(184);t.default=function(){return(0,c.jsx)("div",{children:"Test"})}}}]);
+//# sourceMappingURL=274.0e30e213.chunk.js.map
