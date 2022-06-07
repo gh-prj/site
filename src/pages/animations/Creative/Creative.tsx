a1b7dd2d1378507b88d5ae9585185b24dab42db7
gh-prj <ghprj@yandex.ru>
2022-06-07 18:22:59 +0300

    Add Creative animation

---
+++ src/pages/animations/Creative/Creative.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Creative.module.scss'
+
+const Creative = () => {
+    return (
+        <div className={styles.container}>
+            <h2 data-text="Creative...">Creative...</h2>
+        </div>
+    );
+}
+
+export default Creative;
