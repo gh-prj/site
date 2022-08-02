578de52fe474a708139dc5570105e13f33211fbc
gh-prj <ghprj@yandex.ru>
2022-08-02 17:05:22 +0300

    Updates

---
+++ static/js/2722.68222670.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgh_react_site=self.webpackChunkgh_react_site||[]).push([[2722],{2722:function(e,s,i){i.r(s),i.d(s,{default:function(){return c}});i(2791);var n="Home_container__qRlyu",t=i(184),c=function(){return(0,t.jsxs)("div",{className:n,children:[(0,t.jsx)("div",{}),(0,t.jsx)("div",{}),(0,t.jsx)("div",{}),(0,t.jsx)("div",{}),(0,t.jsx)("div",{})]})}}}]);
+//# sourceMappingURL=2722.68222670.chunk.js.map
